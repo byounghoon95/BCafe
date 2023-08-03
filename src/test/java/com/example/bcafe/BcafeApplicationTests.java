772efc717e7f606bc5b2fd24acf196cad8d59219
owner <toltolyibh@gmail.com>
2023-08-04 00:11:18 +0900

    init

---
+++ src/test/java/com/example/bcafe/BcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
