432fd6d6a7b7567301001efda66cdfed3801684a
bhoon <toltolyibh@gmail.com>
2023-08-05 18:48:36 +0900

    test : MemberApiControllerTest 회원 추가 테스트 코드 추가

---
+++ src/test/java/com/example/bcafe/api/controller/member/MemberApiControllerTest.java
@@ -0,0 +1,55 @@
+package com.example.bcafe.api.controller.member;
+
+import com.example.bcafe.api.controller.member.request.MemberCreateRequest;
+import com.example.bcafe.api.service.member.MemberService;
+import com.example.bcafe.enums.CodeEnum;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ActiveProfiles("test")
+@WebMvcTest
+class MemberApiControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    @MockBean
+    MemberService memberService;
+
+    @DisplayName("새로운 회원 정보를 등록한다")
+    @Test
+    void create_member() throws Exception {
+        //given
+        MemberCreateRequest request = MemberCreateRequest.builder()
+                .phoneNumber("010-1111-2222")
+                .name("이병훈")
+                .age(29)
+                .build();
+
+        //when //then
+        mockMvc.perform(post("/api/member/new")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(jsonPath("$.code").value(CodeEnum.SUCCESS.getCode()))
+        ;
+    }
+
+}
