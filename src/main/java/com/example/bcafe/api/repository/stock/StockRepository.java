9b326aac24483fc1d236af83621a66fdf1ff7f3f
owner <toltolyibh@gmail.com>
2023-08-15 14:41:22 +0900

    feat : 동시 주문 시 에러 막기 위해 Stock 에 비관락 적용

--- src/main/java/com/example/bcafe/api/repository/stock/StockRepository.java
+++ src/main/java/com/example/bcafe/api/repository/stock/StockRepository.java
@@ -2,13 +2,18 @@ package com.example.bcafe.api.repository.stock;
 
 import com.example.bcafe.entity.stock.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
-    List<Stock> findAllByProductCodeIn(List<String> codes);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.productCode IN :productcodes")
+    List<Stock> findAllByProductCodeIn(List<String> productcodes);
     Stock findByProductCode(String productCode);
 }
