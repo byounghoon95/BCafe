487f57f6289223b6e2afbcf9ee334728d76751c7
owner <toltolyibh@gmail.com>
2023-08-17 21:05:34 +0900

    feat : Order관련 DTO와 Controller swagger 기능을 이용 API 명세 추가

--- src/main/java/com/example/bcafe/api/controller/order/OrderApiController.java
+++ src/main/java/com/example/bcafe/api/controller/order/OrderApiController.java
@@ -4,6 +4,8 @@ import com.example.bcafe.api.common.response.CommonResponse;
 import com.example.bcafe.api.controller.order.request.OrderCreateRequest;
 import com.example.bcafe.api.service.order.OrderService;
 import com.example.bcafe.api.service.order.response.OrderResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 
+@Api(tags = "주문 API")
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
 @RestController
@@ -20,6 +23,7 @@ public class OrderApiController {
     private final OrderService orderService;
 
     @PostMapping("/create")
+    @ApiOperation(value = "주문 생성", notes = "핸드폰 번호와 상품번호 리스트를 갖고 주문을 생성한다")
     public CommonResponse<OrderResponse> createOrder(@Valid @RequestBody OrderCreateRequest request) {
         return new CommonResponse<>(orderService.createOrder(request.toServiceRequest(), LocalDateTime.now()));
     }
--- src/main/java/com/example/bcafe/api/controller/order/request/OrderCreateRequest.java
+++ src/main/java/com/example/bcafe/api/controller/order/request/OrderCreateRequest.java
@@ -1,6 +1,8 @@
 package com.example.bcafe.api.controller.order.request;
 
 import com.example.bcafe.api.service.order.request.OrderCreateServiceRequest;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,12 +10,16 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
+@ApiModel(value = "주문 생성 DTO")
 @Getter
 @NoArgsConstructor
 public class OrderCreateRequest {
 
+    @ApiModelProperty(value = "주문한 상품 번호 리스트", notes = "상품 번호 리스트", required = true, example = "['P00001','P00002']")
     @NotEmpty(message = "상품 번호 리스트는 필수입니다")
     private List<String> productCodes;
+
+    @ApiModelProperty(value = "회원 핸드폰 번호", notes = "회원 핸드폰 번호. 비회원인 경우 비회원의 default 번호인 000-0000-0000를 갖는다", example = "010-0000-0000")
     @NotEmpty(message = "핸드폰 번호는 필수입니다")
     private String phoneNumber = "000-0000-0000";
 
